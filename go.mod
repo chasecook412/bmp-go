fba8b0943f97fe02acdd3ba55301070a7de08bd0
tone.chromebook <tonetheman@gmail.com>
2020-05-31 22:11:46 -0500

    added more stuff still not working

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tonetheman/bmp-go
 
 go 1.12
 
-require github.com/elazarl/goproxy v0.0.0-20200426045556-49ad98f6dac1
+require (
+	github.com/elazarl/goproxy v0.0.0-20200426045556-49ad98f6dac1
+	github.com/gorilla/mux v1.7.4
+)
--- main.go
+++ main.go
@@ -2,20 +2,67 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
+	"github.com/gorilla/mux"
 )
+
+var port int
+var verbose bool
+
+func HandleProxyList(res http.ResponseWriter,
+	req *http.Request) {
+
+	// return current list of proxies
+
+}
+
+func HandleProxyDelete(res http.ResponseWriter,
+	req *http.Request) {
+
+	vars := mux.Vars(req)
+	port := vars["port"]
+	fmt.Println("delete proxy with port", port)
+}
+
+func HandleProxyCreate(res http.ResponseWriter,
+	req *http.Request) {
+
+	// port the listen port
+	// proxyUsername
+	// proxyPassword
+	// bindAddress
+	// serverBindAddress
+	// useEcc
+	// trustAllServers
+
+	log.Println("starting a new proxy")
+
+	// create a new proxy pick a new port
+	// how do you pick a port
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = verbose
+	http.ListenAndServe(":8080", proxy)
+	// TODO: need a place to store the proxies
+
+}
 
 func main() {
 
-	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
-	addr := flag.String("addr", ":8080", "proxy listen address")
-	flag.Parse()
+	port = 8081 // default port to start picking
 
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = *verbose
-	log.Fatal(http.ListenAndServe(*addr, proxy))
+	fverbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	if *fverbose {
+		verbose = true
+	} else {
+		verbose = false
+	}
 
+	r := mux.NewRouter()
+	r.HandleFunc("/proxy", HandleProxyList).Methods("GET")
+	r.HandleFunc("/proxy", HandleProxyCreate).Methods("POST")
+	r.HandleFunc("/proxy/{port}", HandleProxyDelete).Methods("DELETE")
 }
